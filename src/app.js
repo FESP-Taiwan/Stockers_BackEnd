70f90cb3305d4ed9b8c41d37e988382a6bf7f954
hohshen <hohshen@gmail.com>
2019-10-11 16:54:22 +0800

    login

--- src/app.js
+++ src/app.js
@@ -1,46 +1,51 @@
-const { ApolloServer } = require('apollo-server-express');
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const jwt = require('jsonwebtoken');
-const { typeDefs, resolvers } = require('./schemas/index');
-const config =require('config')// require('dotenv').config()
+const { ApolloServer } = require("apollo-server-express");
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const jwt = require("jsonwebtoken");
+const { typeDefs, resolvers } = require("./schemas/index");
+const config = require("config"); // require('dotenv').config()
 
 const PORT = process.env.PORT || 5000;
 
-
 const app = express();
-app.get('/',(req,res)=>{res.send('<a href="http://localhost:5000/test">click me</a>')})
-app.use('/',require('./routes/login'));
-app.use('/modules',require('./routes/userModules'))
+app.get("/", (req, res) => {
+  res.send('<a href="http://localhost:5000/test">click me</a>');
+});
+app.use("/", require("./routes/login"));
+app.use("/modules", require("./routes/userModules"));
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    context: async ({req}) => {
-        const token = req.headers['token'];
-        if(token) {
-            try {
-                const user = await jwt.verify(token,config.get('SECRET'));
-                return {user};
-            } catch(e) {
-                throw new Error('Your session expired. Sign in again');
-            }
-        }
-        return {};
+  typeDefs,
+  resolvers,
+  context: async ({ req }) => {
+    const token = req.headers["token"];
+    if (token) {
+      try {
+        const user = await jwt.verify(token, config.get("SECRET"));
+        return { user };
+      } catch (e) {
+        throw new Error("Your session expired. Sign in again");
+      }
     }
-})
+    return {};
+  }
+});
 
-app.get('/test',(req,res) => {
-    res.send('test');
-})
-
+app.get("/test", (req, res) => {
+  res.send("test");
+});
+app.get("/seed", (req, res) => {
+  const { seed } = require("./db/seed/user.seed");
+  seed();
+  res.send("finish");
+});
 server.applyMiddleware({ app });
 
-app.listen(PORT,() => {
-    console.log(`🚀 Server ready at http://localhost:5000${server.graphqlPath}`)
-})
+app.listen(PORT, () => {
+  console.log(`🚀 Server ready at http://localhost:5000${server.graphqlPath}`);
+});
