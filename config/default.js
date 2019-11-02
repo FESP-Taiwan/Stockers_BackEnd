5299cbc133a4b8a12654aa31a9f58fadf779bfd2
justinyeh <justinyeh1995@gmail.com>
2019-11-02 16:36:09 +0800

    FB CANCEL

--- config/default.js
+++ config/default.js
@@ -17,11 +17,7 @@ module.exports = {
     SECRET: "testsecret",
     SALT_ROUNDS: 8,
     login: {
-        fb: {
-            FB_CLIENT_ID: "683948768767075",
-            FB_CLIENT_SECRET: "b1eb1c91231d2341503b5d40d77fd53f",
-            FB_CALLBACK_URL: "https://stockers.ml/auth/facebook/callback",
-        },
+       
         google: {
             GOOGLE_CLIENT_ID: "255901266837-gg8jodm0dfptjjb7hk3h4buio59ma1kt.apps.googleusercontent.com",
             GOOGLE_CLIENT_SECRET: "Wp1XLwrD38D9bIhIZ6r5idQd",
